e93f6b3f3ad2f7a8068fa6b4d7971b09a42a3542
mitchnm <mitchndungu21@gmail.com>
2019-06-28 16:30:45 +0300

    fixed project urls

--- awards/urls.py
+++ awards/urls.py
@@ -1,8 +1,6 @@
 from django.conf.urls import url
 from . import views
-from django.contrib.auth import views
 
 urlpatterns = [
   url(r'^$',views.welcome,name='welcome'),
-  url(r'^logout/$', views.logout, {"next_page": '/'}),
 ]
