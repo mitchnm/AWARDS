c5972224f4ba841816d342dc8753596c8201a750
mitchnm <mitchndungu21@gmail.com>
2019-06-28 16:27:31 +0300

    added registration page

--- awards/urls.py
+++ awards/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from . import views
+from django.contrib.auth import views
 
 urlpatterns = [
   url(r'^$',views.welcome,name='welcome'),
+  url(r'^logout/$', views.logout, {"next_page": '/'}),
 ]
--- awardsproject/settings.py
+++ awardsproject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bootstrap4',
     'awards.apps.AwardsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
