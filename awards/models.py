b83065786c3cbdcb38dbbddbc53d9db334519dba
mitchnm <mitchndungu21@gmail.com>
2019-06-30 15:32:41 +0300

    added classes and created s superuser

--- awards/models.py
+++ awards/models.py
@@ -2,3 +2,19 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    name = models.OneToOneField(User, on_delete=models.CASCADE,primary_key=True)
+    profile_pic = models.ImageField(upload_to = 'awards/')
+    bio = models.CharField(max_length=250)
+    
+    def __str__(self):
+        return self.bio
+    
+    def save_profile(self):
+        self.save()
+
+class Project(models.Model):
+    image = models.ImageField(upload_to = 'awards/')
+    project_name = models.CharField(max_length=60)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    url = models.CharField(max_length=100)
